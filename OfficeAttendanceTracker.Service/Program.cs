787e4c7e6dc8d9d17b44db7259a9a5845fb9ed95
Nelson Chan <khchanel@gmail.com>
2024-05-02 17:58:47 +1000

    add support for Windows Service

--- OfficeAttendanceTracker.Service/Program.cs
+++ OfficeAttendanceTracker.Service/Program.cs
@@ -2,6 +2,10 @@ using OfficeAttendanceTracker.Service;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "Office Attendance Tracker";
+});
 builder.Services.AddTransient<IAttendanceService, AttendanceService>();
 builder.Services.AddTransient<IAttendanceRecordStore, AttendanceRecordFileStore>();
 //builder.Services.AddLogging(logging =>
